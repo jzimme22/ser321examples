fc18698282c8a7314f5b22fda083494c4514fde1
jzimme22 <jzimme22@asu.edu>
2025-03-30 13:00:45 -0600

    Add error handling to WebServer.java

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -216,6 +216,16 @@ class WebServer {
 
           // TODO: Include error handling here with a correct error code and
           // a response that makes sense
+		  if (!(num1 instanceof Integer) || !(num2 instanceof Integer)) {
+			  num1 = 0;
+			  num2 = 0;
+			  result = num1 * num2;
+			  builder.append("HTTP/1.1 400 Bad Request\n");
+			  builder.append("Content-Type: text/html; charset=utf-8\n");
+              builder.append("\n");
+              builder.append("Result is: " + result);
+			  
+		  }
 
         } else if (request.contains("github?")) {
           // pulls the query from the request and runs it with GitHub's REST API
